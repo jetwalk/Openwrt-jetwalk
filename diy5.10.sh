9972f6031df83e4e6faaf117b27e7fc2c6cb9866
jetwalk <69178819+jetwalk@users.noreply.github.com>
2022-07-04 09:43:00 +0800

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -32,6 +32,6 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 # git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
-svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
