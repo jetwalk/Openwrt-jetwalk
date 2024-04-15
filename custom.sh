602bcfd237f4e5f018169716fb2a258324735e4b
jetwalk <69178819+jetwalk@users.noreply.github.com>
2024-04-15 09:17:16 +0800

    Add files via upload

---
+++ custom.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.50.70/g' package/base-files/files/bin/config_generate
+ 
+# add custom packages
+# echo 'src-git xYx https://github.com/xYx-c/openwrt-luci' >>feeds.conf.default
+
+# openclash packages
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git openclash
+mv openclash/luci-app-openclash package/luci-app-openclash && rm -rf openclash
+
+git clone --depth=1 -b master https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+git clone --depth=1 -b main https://github.com/kingyond/luci-app-accesscontrol-plus accesscontrol-plus
+mv accesscontrol-plus/luci-app-accesscontrol-plus  package/luci-app-accesscontrol-plus && rm -rf accesscontrol-plus
+
+git clone --depth=1 -b master https://github.com/rufengsuixing/luci-app-zerotier package/luci-app-zerotier
+
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+
+# update golang
+# pushd feeds/packages/lang
+# rm -rf golang && svn co https://github.com/openwrt/packages/trunk/lang/golang
+# popd
