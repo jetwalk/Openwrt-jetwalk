7ca975f5f6e10c03f3fa6df462b48becd9f527d3
jetwalk <69178819+jetwalk@users.noreply.github.com>
2024-12-17 08:11:40 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -55,10 +55,9 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
-#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-#find ./ | grep Makefile | grep mosdns | xargs rm -f
-
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
 #git clone https://github.com/sbwml/luci-app-dae package/dae
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
